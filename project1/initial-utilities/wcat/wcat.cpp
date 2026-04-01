d4864804bf82d5bf8459de60595450f78c67575e
Adrian Perez <appperez@ucdavis.edu>
2026-04-01 20:55:18 +0000

    Add gitignore and wcat implementation

---
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+int main(int argc, char* argv[]) {
+
+  // start at one because the first argument is always the name of the program
+  for (int i = 1; i < argc; i ++) {
+    const char *file_name = argv[i];
+    int f = open(file_name, O_RDONLY);
+
+    if (f < 0) { // the read was not successful
+      std::cout << "wcat: cannot open file" << std::endl;
+      exit(1);
+    }
+    
+    size_t buff_size = 10;
+    char *buf = (char *) calloc(sizeof(char), buff_size);
+
+    int bytes_read = 0;
+    while ((bytes_read = read(f, buf, buff_size)) > 0){
+      write(STDOUT_FILENO, buf, bytes_read);
+    }
+
+
+    close(f);
+  }
+
+  return 0;
+}
