945b727f2815617cd8873515f51e751714dda381
Adrian Perez <appperez@ucdavis.edu>
2026-04-05 00:57:38 +0000

    add wunzip

---
+++ project1/initial-utilities/wunzip/wunzip.cpp
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string> 
+
+int main (int argc, char *argv[]) {
+  std::string err_msg;
+  uint32_t appearances;
+  char c;
+  int byte_read = 1;
+
+  if (argc < 2) {
+    err_msg = "wunzip: file1 [file2 ...]\n";
+    write(STDOUT_FILENO, err_msg.c_str(), err_msg.size());
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    int f = open(argv[i], O_RDONLY);
+
+    while ((byte_read = read(f, &appearances, 4)) > 0) {
+      read(f, &c, 1);
+      for (int j = 0; j < (int) appearances; j++) {
+        write(STDOUT_FILENO, &c, 1);
+      }
+    }
+
+    close(f);
+  }
+
+  return 0;
+}
