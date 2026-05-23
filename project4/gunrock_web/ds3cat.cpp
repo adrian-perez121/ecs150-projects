676094fcb05d42e54efa72c0b4de9069e14ff099
Adrian Perez <appperez@ucdavis.edu>
2026-05-23 17:25:00 +0000

    implement ds3cat

--- project4/gunrock_web/ds3cat.cpp
+++ project4/gunrock_web/ds3cat.cpp
@@ -6,6 +6,7 @@
 #include "LocalFileSystem.h"
 #include "Disk.h"
 #include "ufs.h"
+#include <vector>
 
 using namespace std;
 
@@ -17,11 +18,49 @@ int main(int argc, char *argv[]) {
   }
 
   // Parse command line arguments
-  /*
   Disk *disk = new Disk(argv[1], UFS_BLOCK_SIZE);
   LocalFileSystem *fileSystem = new LocalFileSystem(disk);
   int inodeNumber = stoi(argv[2]);
-  */
 
+  inode_t inode;
+  if (fileSystem->stat(inodeNumber, &inode) != 0) {
+    delete fileSystem;
+    delete disk;
+    cerr << "Error reading file" << endl;
+    return 1;
+  }
+
+  if (inode.type == UFS_DIRECTORY) {
+    delete fileSystem;
+    delete disk;
+    cerr << "Error reading file" << endl;
+    return 1;
+  }
+
+  int num_blocks = (inode.size + (UFS_BLOCK_SIZE - 1)) / UFS_BLOCK_SIZE;
+  vector<unsigned char> data(inode.size);
+
+  if (fileSystem->read(inodeNumber, data.data(), inode.size) != 0) {
+    delete fileSystem;
+    delete disk;
+    cerr << "Error reading file" << endl;
+    return 1;
+  }
+
+  cout << "File blocks" << endl;
+  for (int i = 0; i < num_blocks; i++) {
+    cout << inode.direct[i] << endl;
+  }
+
+  cout << endl;
+
+  cout << "File data" << endl;
+
+  for (auto byte : data) {
+    cout << byte;
+  }
+
+  delete fileSystem;
+  delete disk;
   return 0;
 }
