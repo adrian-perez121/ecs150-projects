cb92c0a39993b6e01b579ec0a284cef9be06c286
Adrian Perez <appperez@ucdavis.edu>
2026-05-23 16:57:21 +0000

    Implement ds3bits

--- project4/gunrock_web/ds3bits.cpp
+++ project4/gunrock_web/ds3bits.cpp
@@ -6,6 +6,7 @@
 #include "LocalFileSystem.h"
 #include "Disk.h"
 #include "ufs.h"
+#include <vector>
 
 using namespace std;
 
@@ -17,10 +18,50 @@ int main(int argc, char *argv[]) {
   }
 
   // Parse command line arguments
-  /*
   Disk *disk = new Disk(argv[1], UFS_BLOCK_SIZE);
   LocalFileSystem *fileSystem = new LocalFileSystem(disk);
-  */
+
+  super_t super;
+  fileSystem->readSuperBlock(&super);
+
+
+  cout << "Super" << endl;
+
+  cout << "inode_region_addr "  << super.inode_region_addr << endl;
+
+  cout << "inode_region_len " << super.inode_region_len << endl;
+
+  cout << "num_inodes " << super.num_inodes << endl;
+
+  cout << "data_region_addr " << super.data_region_addr << endl;
+
+  cout << "data_region_len " << super.data_region_len << endl;
+
+  cout << "num_data " << super.num_data << endl;
+
+  cout << endl;
+
+  cout << "Inode bitmap" << endl;
+  vector<unsigned char> inode_bitmap(super.num_inodes / 8);
+  fileSystem->readInodeBitmap(&super, inode_bitmap.data());
+
+  for (auto byte : inode_bitmap) {
+    cout << (unsigned int) byte << " ";
+  }
+
+  cout << endl << endl;
+
+  cout << "Data bitmap" << endl;
+  vector<unsigned char> data_bitmap(super.num_data / 8);
+  fileSystem->readDataBitmap(&super, data_bitmap.data());
+
+  for (auto byte : data_bitmap) {
+    cout << (unsigned int) byte << " ";
+  }
+
+  cout << endl;
   
+  delete fileSystem;
+  delete disk;
   return 0;
 }
